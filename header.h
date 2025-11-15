5499140f45cbd709a13ddfb18ea4a6be6421d686
HashiramaSenjuhari <truebillionhari@gmail.com>
2025-11-15 22:45:55 +0530

    Added Header File

---
+++ header.h
@@ -0,0 +1,24 @@
+#ifndef GREAT_H // checks GREAT_H if not defined
+#define GREAT_H // define GREAT_H
+
+void billionaire(const char *file);
+
+#endif // marks the end of conditional section started by #ifndef
+
+#ifndef GREATS_H
+#define GREATS_H
+
+void billionaires();
+
+#endif
+
+
+//  code usage
+//
+//  #include "great.h"
+//
+//  void billionaire(const char *file){
+//    
+//  }
+//
+//  void billionaires(){}
